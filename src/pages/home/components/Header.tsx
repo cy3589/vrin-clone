98f62364a2bb5542c33430a987c41bb8f02d45fc
chad <jcy3589@gmail.com>
2024-04-10 23:06:50 +0900

    feat: Header(larger then mobile), section1

---
+++ src/pages/home/components/Header.tsx
@@ -0,0 +1,206 @@
+import styled from '@emotion/styled';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Logo from './Logo';
+
+import { Button as MUIButton } from '@mui/material';
+const GetStartButton = styled(MUIButton)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 20px;
+  border-radius: 4px;
+  color: rgb(255, 255, 255);
+  background-color: rgb(55, 55, 204);
+  & > span {
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 140%;
+    letter-spacing: 0px;
+  }
+`;
+
+const LanguageChangerButton = styled(MUIButton)`
+  width: 80px;
+  height: 32px;
+  color: rgb(255, 255, 255);
+  font-size: 1.8rem;
+  font-weight: 600;
+  line-height: 140%;
+  letter-spacing: 0px;
+
+  @media only screen and (max-width: 1024px) {
+    margin-right: 24px;
+  }
+`;
+
+const useHeaderTexts = () => {
+  const { t } = useTranslation('main');
+  const pricing = t('header.pricing');
+  const getStartedForFree = t('header.getStartedForFree');
+  const freeCopy = t('header.freeCopy');
+  const login = t('header.login');
+  return { pricing, getStartedForFree, freeCopy, login };
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 70px;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: 999;
+  background: rgba(0, 0, 0, 0.5);
+`;
+const Container = styled.div`
+  padding: 0px 80px;
+  height: 100%;
+  @media only screen and (max-width: 1024px) {
+    padding: 0px 70px;
+  }
+`;
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  a {
+    color: rgb(255, 255, 255);
+  }
+`;
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0px;
+  margin-left: 20px;
+  gap: 60px;
+  & > * {
+    color: white;
+    font-size: 1.8rem;
+    font-weight: 600;
+    line-height: 140%;
+    letter-spacing: 0px;
+    cursor: pointer;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    gap: 32px;
+  }
+  @media only screen and (max-width: 768px) {
+    gap: 24px;
+  }
+`;
+
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
+import ArrowDown from './ArrowDown';
+const ArrowDownWrapper = styled.div`
+  position: absolute;
+  top: 4px;
+  right: -6px;
+  color: rgb(111, 117, 123);
+  & > svg {
+    fill: currentColor;
+  }
+`;
+
+const HeaderMenuItem = styled(MenuItem)`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 20px;
+  width: 100%;
+  cursor: pointer;
+  color: rgb(255, 255, 255);
+`;
+
+import CheckIcon from '@mui/icons-material/Check';
+import { css } from '@emotion/react';
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const onOpen = (e: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(e.currentTarget);
+  const onClose = () => setAnchorEl(null);
+
+  const { getStartedForFree, pricing, login } = useHeaderTexts();
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language === 'KR' ? 'KOR' : 'ENG';
+  return (
+    <>
+      <Wrapper style={{ height: '70px' }}>
+        <Container>
+          <InnerContainer style={{ height: '100%', textAlign: 'center' }}>
+            <Link to="/">
+              <Logo />
+            </Link>
+            <Right>
+              <div>{pricing}</div>
+              <LanguageChangerButton
+                disableRipple
+                css={css`
+                  width: 80px;
+                  height: 32px;
+                  color: rgb(255, 255, 255);
+                  font-size: 1.8rem;
+                  font-weight: 600;
+                  line-height: 140%;
+                  letter-spacing: 0px;
+                `}
+                id="language-changer-button"
+                aria-controls={open ? 'language-changer-menu' : undefined}
+                aria-haspopup
+                aria-expanded={open ? 'true' : undefined}
+                onClick={onOpen}>
+                {currentLanguage}
+                <ArrowDownWrapper>
+                  <ArrowDown width={24} height={24} />
+                </ArrowDownWrapper>
+              </LanguageChangerButton>
+              <div style={{ color: 'rgb(110, 133, 252)' }}>{login}</div>
+              <GetStartButton>
+                <span>{getStartedForFree}</span>
+              </GetStartButton>
+            </Right>
+          </InnerContainer>
+        </Container>
+      </Wrapper>
+      <Menu
+        id="language-changer-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={onClose}
+        slotProps={{ paper: { style: { backgroundColor: 'rgb(40, 45, 50)', width: '138px', marginTop: '8px' } } }}
+        MenuListProps={{ 'aria-labelledby': 'language-changer-button' }}>
+        <HeaderMenuItem
+          onClick={() => {
+            i18n.changeLanguage('KR');
+            onClose();
+          }}>
+          <span style={{ color: i18n.language === 'KR' ? 'rgb(110, 133, 252)' : undefined }}>KOR</span>
+          {i18n.language === 'KR' && (
+            <div>
+              <CheckIcon htmlColor="rgb(110, 133, 252)" />
+            </div>
+          )}
+        </HeaderMenuItem>
+        <HeaderMenuItem
+          onClick={() => {
+            i18n.changeLanguage('US');
+            onClose();
+          }}>
+          <span style={{ color: i18n.language === 'US' ? 'rgb(110, 133, 252)' : undefined }}>ENG</span>
+          {i18n.language === 'US' && (
+            <div>
+              <CheckIcon htmlColor="rgb(110, 133, 252)" />
+            </div>
+          )}
+        </HeaderMenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/components/Section1.tsx
@@ -0,0 +1,157 @@
+import { useTranslation } from 'react-i18next';
+
+const useSection1Texts = () => {
+  const { t } = useTranslation('main');
+  const title = t('section1.title');
+  const descriptionHTML = t('section1.descriptionHTML');
+  const getStartedForFree = t('section1.getStartedForFree');
+  return { title, descriptionHTML, getStartedForFree };
+};
+
+import styled from '@emotion/styled';
+import { Button as MUIButton } from '@mui/material';
+const Container = styled.div`
+  background-color: rgb(5, 7, 14);
+  background-image: url(/assets/vrin-home_A-1_PC.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  @media only screen and (max-width: 1024px) {
+    background-image: url(/assets/vrin-home_A-1_laptop.png);
+  }
+  @media only screen and (max-width: 768px) {
+    background-image: url(/assets/vrin-home_A-1_tablet.png);
+  }
+  @media only screen and (max-width: 600px) {
+    background-image: url(/assets/vrin-home_A-1_mob.png);
+  }
+`;
+const InnerContainer = styled.div`
+  position: relative;
+  max-width: 1300px;
+  width: calc(100% - 180px);
+  height: 100vh;
+  max-height: 970px;
+  z-index: 1;
+  white-space: pre-wrap;
+
+  margin: 0 auto;
+  box-sizing: border-box;
+  display: block;
+  padding-left: 16px;
+  padding-right: 16px;
+
+  @media only screen and (max-width: 1024px) {
+    max-width: 844px;
+    max-height: 870px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    max-width: 630px;
+    width: calc(100% - 140px);
+    max-height: 540px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    width: calc(100% - 40px);
+    max-height: 420px;
+  }
+`;
+
+const DescriptionBox = styled.div`
+  position: absolute;
+  left: 0px;
+  bottom: 120px;
+  z-index: 2;
+  & > span {
+    color: rgb(189, 193, 199);
+    font-size: 3.2rem;
+    font-weight: 400;
+    line-height: 140%;
+    @media only screen and (max-width: 1024px) {
+      font-size: 2.4rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+      font-size: 1.6rem;
+    }
+  }
+  & > div:first-of-type {
+    color: rgb(230, 232, 235);
+    font-size: 5.2rem;
+    font-weight: 700;
+    line-height: normal;
+    margin-bottom: 12px;
+    @media only screen and (max-width: 768px) {
+      font-size: 4.4rem;
+    }
+    @media only screen and (max-width: 600px) {
+      font-size: 2.4rem;
+      margin-bottom: 10px;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    bottom: 90px;
+  }
+  @media only screen and (max-width: 600px) {
+    bottom: 70px;
+  }
+`;
+const Button = styled(MUIButton)`
+  width: 100%;
+  max-width: 326px;
+  padding: 16px;
+  margin-top: 56px;
+  border-radius: 6px;
+  background-color: rgb(55, 55, 204);
+  color: rgb(255, 255, 255);
+  & > span {
+    font-size: 2.4rem;
+    font-weight: 600;
+    line-height: 140%;
+    @media only screen and (max-width: 1024px) {
+      font-size: 2rem;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 1.8rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media only screen and (max-width: 1024px) {
+    max-width: 243px;
+    margin-top: 48px;
+  }
+  @media only screen and (max-width: 768px) {
+    max-width: 210px;
+    padding: 12px;
+    margin-top: 40px;
+  }
+  @media only screen and (max-width: 600px) {
+    max-width: 160px;
+    padding: 10px;
+    margin-top: 24px;
+    border-radius: 4px;
+  }
+`;
+const Section1 = () => {
+  const { getStartedForFree, descriptionHTML, title } = useSection1Texts();
+  return (
+    <Container>
+      <InnerContainer>
+        <DescriptionBox>
+          <div>{title}</div>
+          <span dangerouslySetInnerHTML={{ __html: descriptionHTML }} />
+          <Button>
+            <span>{getStartedForFree}</span>
+          </Button>
+        </DescriptionBox>
+      </InnerContainer>
+    </Container>
+  );
+};
+
+export default Section1;
